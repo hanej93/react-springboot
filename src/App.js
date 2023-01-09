878012b39790310ba490b7b2524f418d365ee717
EJ <hanej93@naver.com>
2023-01-09 18:27:54 +0900

    React useMemo() 실습

--- src/App.js
+++ src/App.js
@@ -1,43 +1,40 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [data, setData] = useState(0);
-  const [search, setSearch] = useState(0);
+  const [list, setList] = useState([1, 2, 3, 4]);
+  const [str, setStr] = useState('합계');
 
-  const download = () => {
-    // 다운로드 받고 (통신)
-    let donwnloadData = 5; // 가정
-    setData(donwnloadData);
+  const getAddResult = () => {
+    console.log('SUM 함수실행됨');
+    return list.reduce((a, b) => a + b);
   };
 
-  // 실행시점 :
-  // (1) App() 함수가 최초 실행될 때(마운트될 때) -> 그림이 최초 그려질 때
-  // (2) 상태 변수가 변경될 때(그게 dependencyList에 등록되어 있어야 함)
-  // (3) 의존리스트 관리를 할 수 있다.
-  useEffect(() => {
-    console.log('useEffect 실행됨');
-    download();
-  }, [search]);
+  const addResult = useMemo(() => getAddResult(), [list]);
 
   return (
     <>
+      <button
+        onClick={() => {
+          setStr('안녕');
+        }}
+      >
+        문자변경
+      </button>
+      <button
+        onClick={() => {
+          setList([...list, 10]);
+        }}
+      >
+        리스트값 추가
+      </button>
       <div>
-        <button
-          onClick={() => {
-            setSearch(search + 1);
-          }}
-        >
-          검색하기
-        </button>
-        <h1>데이터 : {data}</h1>
-        <button
-          onClick={() => {
-            setData(data + 1);
-          }}
-        >
-          더하기
-        </button>
+        {list.map((v) => (
+          <h1>{v}</h1>
+        ))}
+      </div>
+      <div>
+        {str} : {addResult}
       </div>
     </>
   );
