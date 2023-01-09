625fe0ec3820f6118c6cbfb6b04b1464b2204339
EJ <hanej93@naver.com>
2023-01-09 16:50:00 +0900

    react useState() 연습

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Sub from './Sub';
 
 // 0. React 엔진 - 데이터 변경 감지에서 UI 그려주는!!
 // 1. 실행과정(index.html) -> SPA(Single Page Application)
@@ -15,13 +16,37 @@ import './App.css';
 //      - 라이브러리 사용(부트스트랩, component-styled)
 
 function App() {
-  let list = [1, 2, 3];
+  console.log('앱 실행됨');
+
+  const [num, setNum] = useState(5);
+
+  let sample = [
+    { id: 1, name: '홍길동' },
+    { id: 2, name: '임꺽정' },
+    { id: 3, name: '장보고' },
+    { id: 4, name: '코스' },
+  ];
+
+  const [users, setUsers] = useState(sample); // 레퍼런스 변경되야 동작!!
+
+  const download = () => {
+    //fetch().then().then()
+    setUsers([...sample, { id: num, name: '조자룡' }]);
+    setNum(num + 1);
+  };
+
+  // 랜더링 시점 = 상태값 변경
   return (
     <>
       <div>
-        {list.map((n) => (
-          <h1>{n}</h1>
-        ))}
+        <button onClick={download}>다운로드</button>
+        {users.map((u) => {
+          return (
+            <h1>
+              {u.id} : {u.name}
+            </h1>
+          );
+        })}
       </div>
     </>
   );
