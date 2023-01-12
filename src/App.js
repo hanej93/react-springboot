cf4c478a8c32316be1227cacfdd3e539538a59f7
EJ <hanej93@naver.com>
2023-01-12 10:36:40 +0900

    1. 불필요한 파일 정리 2. components 화(header, footer ..)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
 import styled from 'styled-components';
-import './App.css';
-
-// useRef (디자인)
-// dom을 변경할 때 사용
-
-const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
-`;
+import LoginPage from './pages/LoginPage';
 
 function App() {
-  return (
-    <div>
-      <Title>헬로</Title>
-    </div>
-  );
+  return <LoginPage />;
 }
 
 export default App;
