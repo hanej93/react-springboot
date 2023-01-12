93b872c7f624dc869d7c219dad39d2a4e71e94f4
EJ <hanej93@naver.com>
2023-01-12 12:07:53 +0900

    styled-component passing

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledDeleteButton = styled.button`
+  color: ${(props) => (props.user.username === 'ssar' ? 'blue' : 'red')};
+`;
 
 // Function 방식
 const Home = (props) => {
   //   const boards = props.boards;
 
   // 구조분할 할당
-  const { boards, setBoards } = props;
-  const { number, setNumber } = props;
+  const { boards, setBoards, user } = props;
 
   return (
     <div>
-      <h1>홈{number}</h1>
-      <button onClick={() => setNumber(number + 1)}>번호증가</button>
-      <button onClick={() => setBoards([])}>전체삭제</button>
+      <h1>홈페이지</h1>
+      <StyledDeleteButton user={user} onClick={() => setBoards([])}>
+        전체삭제
+      </StyledDeleteButton>
       {boards.map((board) => {
         return (
           <h3>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,7 +6,7 @@ import Home from '../components/home/Home';
 const HomePage = () => {
   // http 요청 (fetch, axios(다운))
   const [boards, setBoards] = useState([]);
-  const [number, setNumber] = useState(0);
+  const [user, setUser] = useState({});
 
   useEffect(() => {
     // 다운로드 가정 = fetch(), axios(), ajax()
@@ -18,17 +18,13 @@ const HomePage = () => {
 
     // 빈데이터
     setBoards([...data]);
+    setUser({ id: 1, username: 'ssar' });
   }, []); // 빈 배열 최초에 한번만 실행
 
   return (
     <div>
       <Header />
-      <Home
-        boards={boards}
-        setBoards={setBoards}
-        number={number}
-        setNumber={setNumber}
-      />
+      <Home boards={boards} setBoards={setBoards} user={user} />
       <Footer />
     </div>
   );
