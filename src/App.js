cb218457c8d310ee2a6f0fce59906d38ae980465
EJ <hanej93@naver.com>
2023-01-16 12:10:31 +0900

    글쓰기, 글목록보기 실습 2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Route, Routes } from 'react-router-dom';
-import Navigation from './components/Navigation';
 import ListPage from './pages/ListPage';
-import WritePage from './pages/WritePage';
 
 // 글쓰기, 글삭제, 글목록보기
 
 function App() {
   return (
     <>
-      <Navigation />
-      <Routes>
-        <Route path="/" exact element={<ListPage />} />
-        <Route path="/write" exact element={<WritePage />} />
-      </Routes>
+      <ListPage />
     </>
   );
 }
--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -12,22 +12,48 @@ const StyledItemBoxDiv = styled.div`
 `;
 
 const ListPage = () => {
+  const [no, setNo] = useState(6);
+
+  const [post, setPost] = useState({
+    id: no,
+    title: '',
+    content: '',
+  });
+
   const [posts, setPosts] = useState([
-    { id: 1, title: '내용1' },
-    { id: 2, title: '내용2' },
-    { id: 3, title: '내용3' },
-    { id: 4, title: '내용4' },
-    { id: 5, title: '내용5' },
+    { id: 1, title: '제목1', content: '내용1' },
+    { id: 2, title: '제목2', content: '내용2' },
+    { id: 3, title: '제목3', content: '내용3' },
+    { id: 4, title: '제목4', content: '내용4' },
+    { id: 5, title: '제목5', content: '내용5' },
   ]);
+
+  const handleWrite = (e) => {
+    e.preventDefault(); // form태그가 하려는 액션을 중지
+    setPosts([...posts, post]);
+    setNo(no + 1);
+    setPost({ ...post, id: no });
+  };
+
+  const handleForm = (e) => {
+    // compute property names (key 값을 동적할당)
+    setPost({ ...post, [e.target.name]: e.target.value, id: no });
+    console.log(`제목: ${post.title} // 내용: ${post.content}`);
+  };
 
   return (
     <div>
       <h1>리스트페이지</h1>
+      <form onSubmit={handleWrite}>
+        <input type="text" placeholder="제목을 입력하세요.." value={post.title} name="title" onChange={handleForm} />
+        <input type="text" placeholder="내용을 입력하세요.." value={post.content} name="content" onChange={handleForm} />
+        <button type="submit">글쓰기</button>
+      </form>
       <hr />
       {posts.map((post) => (
-        <StyledItemBoxDiv>
+        <StyledItemBoxDiv key={post.id}>
           <div>
-            번호: {post.id} 제목: {post.title}
+            NO: {post.id} TITLE: {post.title} CONTENT: {post.content}
           </div>
           <button>삭제</button>
         </StyledItemBoxDiv>
