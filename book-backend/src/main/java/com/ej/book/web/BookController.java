a6ce108f447c57c69c7dedfdb4a096f80e083584
ej <ej@happytalk.io>
2023-01-17 12:58:54 +0900

    react 책 리스트 조회

--- book-backend/src/main/java/com/ej/book/web/BookController.java
+++ book-backend/src/main/java/com/ej/book/web/BookController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,6 +30,7 @@ public class BookController {
 		return new ResponseEntity<>(newBook, HttpStatus.CREATED); // 201
 	}
 
+	@CrossOrigin
 	@GetMapping("/book")
 	public ResponseEntity<?> findAll() {
 		List<Book> books = bookService.모두가져오기();
--- book-frontend/src/pages/book/Home.js
+++ book-frontend/src/pages/book/Home.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import BookItem from '../../components/BookItem';
 
 const Home = () => {
+  const [books, setBooks] = useState([]);
+
+  // 함수 실행시 최초 한번 실행되는 것 useEffect
+  //  + 상태값이 변경될 때마다 실행
+  // ※ 의존성이 없을 경우 무한 실행
+  useEffect(() => {
+    // javascript 비동기 함수
+    fetch('http://localhost:8080/book', {
+      method: 'GET',
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        setBooks(res);
+      });
+  }, []);
+
   return (
     <div>
-      <h1>책 리스트 보기</h1>
+      {books.map((book, idx) => (
+        <BookItem key={book.id} book={book} />
+      ))}
     </div>
   );
 };
